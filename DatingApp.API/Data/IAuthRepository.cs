39d3a1c7856a5b80ad22a5576dfd3e5d16b70edd
Serge <kusnir.sergiu@gmail.com>
2020-02-20 18:35:35 +0200

    Angular Client Side

--- DatingApp.API/Data/IAuthRepository.cs
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace DatingApp.API
-{
-    public interface IAuthRepository
-    {
-        
-    }
-}
+﻿using System.Threading.Tasks;
+ using DatingApp.API.Models;
+ 
+ namespace DatingApp.API
+ {
+     public interface IAuthRepository
+     {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+     }
+ }
